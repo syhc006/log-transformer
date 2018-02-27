d2da1f99b35b9e114861c45fdff5efac9b5ca39c
sunyu <sunyu@ht706.com>
2018-02-27 09:51:22 +0800

    修改打印解析日志功能

--- src/main/java/net/xicp/chocolatedisco/logtransformer/function/Printer.java
+++ src/main/java/net/xicp/chocolatedisco/logtransformer/function/Printer.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.storm.trident.operation.BaseFunction;
 import org.apache.storm.trident.operation.TridentCollector;
 import org.apache.storm.trident.tuple.TridentTuple;
+import org.apache.storm.tuple.Values;
 
 /**
  * Created by SunYu on 2018/2/9.
@@ -14,6 +15,6 @@ public class Printer extends BaseFunction {
     @Override
     public void execute(TridentTuple tuple, TridentCollector collector) {
         log.debug(JSON.toJSONString(tuple.get(0)));
-        System.out.println(JSON.toJSONString(tuple.get(0)));
+        collector.emit(new Values(tuple.get(0)));
     }
 }
